b3310dc53a298ccbc922a72befd4cba36103f28f
Phil Freeman <paf31@cantab.net>
2015-08-16 14:36:45 -0700

    Use fold, not do

--- test/Main.purs
+++ test/Main.purs
@@ -3,6 +3,7 @@ module Test.Main where
 import Prelude
 
 import Data.Maybe
+import Data.Foldable (fold)
 
 import Control.Monad.Eff
 
@@ -13,16 +14,15 @@ main = do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
 
-  let bind x f = x <> f unit
-
-  render ctx do
-    outlined (outlineColor red <> lineWidth 5.0) $ 
-      rectangle 20.0 20.0 50.0 50.0
-    clipped (rectangle 20.0 20.0 80.0 80.0) $ 
-      filled (fillColor lightGreen) $ 
-        circle 100.0 100.0 50.0
-    outlined (outlineColor blue <> lineWidth 3.0) $ 
-      closed [ { x: 90.0,  y: 80.0 }
-             , { x: 120.0, y: 120.0 }
-             , { x: 60.0,  y: 120.0 }
-             ]
+  render ctx $ fold
+    [ outlined (outlineColor red <> lineWidth 5.0) $ 
+        rectangle 20.0 20.0 50.0 50.0
+    , clipped (rectangle 20.0 20.0 80.0 80.0) $ 
+        filled (fillColor lightGreen) $ 
+          circle 100.0 100.0 50.0
+    , outlined (outlineColor blue <> lineWidth 3.0) $ 
+        closed [ { x: 90.0,  y: 80.0 }
+               , { x: 120.0, y: 120.0 }
+               , { x: 60.0,  y: 120.0 }
+               ]
+    ]
