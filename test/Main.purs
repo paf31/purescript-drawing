101970a9baff17596546a7f906cd340c8b76f6df
Phil Freeman <paf31@cantab.net>
2015-08-24 12:23:21 -0700

    better example

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,9 @@ module Test.Main where
 
 import Prelude
 
+import Data.Int
 import Data.Maybe
+import Data.Array ((..))
 import Data.Monoid (mempty)
 import Data.Foldable (fold)
 
@@ -15,21 +17,18 @@ main = do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
 
-  render ctx $ fold
-    [ shadow (shadowColor black <> shadowBlur 10.0) $
-        outlined (outlineColor red <> lineWidth 5.0) $ 
-          rectangle 10.0 10.0 90.0 90.0
-    , translate 120.0 0.0 $ 
-        clipped (rectangle 0.0 0.0 50.0 50.0 <> rectangle 50.0 50.0 50.0 50.0) $ 
-          filled (fillColor (lighten 0.5 red)) $ 
-            circle 50.0 50.0 50.0
-    , translate 240.0 0.0 $ 
-        shadow (shadowColor black <> shadowBlur 10.0) $
-          outlined (outlineColor (darken 0.5 red) <> lineWidth 3.0) $ 
-            closed [ { x: 50.0,  y: 10.0 }
-                   , { x: 0.0,   y: 100.0 }
-                   , { x: 100.0, y: 100.0 }
-                   ]
-    , shadow (shadowColor black <> shadowBlur 2.0) $
-        text (font cursive 24 mempty) 10.0 140.0 (fillColor red) "purescript-drawing" 
-    ]
+  render ctx $ translate 400.0 400.0 $ scale 200.0 200.0 $ go 5
+  where
+  s = 0.375
+      
+  go 0 = mempty
+  go n = 
+    let dr = scale s s (go (n - 1))
+    in filled (fillColor (lighten (1.0 - toNumber n / 6.0) black)) (closed pentagon) 
+       <> fold do i <- 0..5
+                  return (rotate (Math.pi / 2.5 * (toNumber i + 0.5)) (translate 0.0 (Math.cos (Math.pi / 5.0) * (1.0 + s)) dr))
+
+  pentagon = do
+    i <- 0..5
+    let theta = Math.pi / 2.5 * toNumber i
+    return { x: Math.sin theta, y: Math.cos theta }
